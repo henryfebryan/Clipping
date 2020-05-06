e533dcc86a69517cb7753d4e8486c3de7372d026
henryfebryan24 <henry24febryan@gmail.com>
2020-05-06 09:14:53 +0700

    Set Up shapes, Row and Column Locations

--- app/src/main/java/com/henry/clipping/ClippedView.kt
+++ app/src/main/java/com/henry/clipping/ClippedView.kt
@@ -21,4 +21,26 @@ class ClippedView @JvmOverloads constructor(
 
     private val path = Path()
 
+    private val clipRectRight = resources.getDimension(R.dimen.clipRectRight)
+    private val clipRectBottom = resources.getDimension(R.dimen.clipRectBottom)
+    private val clipRectTop = resources.getDimension(R.dimen.clipRectTop)
+    private val clipRectLeft = resources.getDimension(R.dimen.clipRectLeft)
+
+    private val rectInset = resources.getDimension(R.dimen.rectInset)
+    private val smallRectOffset = resources.getDimension(R.dimen.smallRectOffset)
+
+    private val circleRadius = resources.getDimension(R.dimen.circleRadius)
+
+    private val textOffset = resources.getDimension(R.dimen.textOffset)
+    private val textSize = resources.getDimension(R.dimen.textSize)
+
+    private val columnOne = rectInset
+    private val columnTwo = columnOne + rectInset + clipRectRight
+
+    private val rowOne = rectInset
+    private val rowTwo = rowOne + rectInset + clipRectBottom
+    private val rowThree = rowTwo + rectInset + clipRectBottom
+    private val rowFour = rowThree + rectInset + clipRectBottom
+    private val textRow = rowFour + (1.5f * clipRectBottom)
+
 }
